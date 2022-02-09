232b805bd10cc0366a9192fe9a1d5642289906d3
dev-rahim <rahimuddin5915@gmail.com>
2022-02-09 23:06:47 +0600

    ecplore function multiple perammitars

---
+++ multiple-peramitar.js
@@ -0,0 +1,24 @@
+function summetionTowNumbers( num1, num2 ) {
+    // console.log( 'summetion is=', num1 + num2 );
+    var total = num1 + num2;
+    return total;
+}
+summetionTowNumbers( 20, 80 );
+
+number1 = 500;
+number2 = 300;
+summetionTowNumbers( number1, number2 );
+
+var result = summetionTowNumbers( 10, 52 );
+console.log( result );
+
+
+
+function multiplyTowNumber( number1, number2 ) {
+    var multiplication = number1 * number2;
+    return multiplication;
+}
+
+var multiplicationOfTowNumbers = multiplyTowNumber( 5, 2 );
+console.log( multiplicationOfTowNumbers );
+
