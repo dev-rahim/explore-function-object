4da77b17c869075664b2323a03430bbd1e9c5315
dev-rahim <rahimuddin5915@gmail.com>
2022-02-10 01:31:37 +0600

    exploring simple  object

---
+++ object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id: 115,
+    name: 'Ábir Adnan',
+    marks: 81,
+    phone: 221211
+};
+
+var mobile = {
+    color: 'red',
+    price: 19000,
+    screenSize: 5,
+    storage: '16 gb'
+};
+
+var book = {
+    title: 'Start With Why',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 246,
+    publisher: 'penguin',
+};
